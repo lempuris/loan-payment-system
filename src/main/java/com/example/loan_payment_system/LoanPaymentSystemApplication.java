8010d3c57703141f8acbd6c0337592d14aabd74a
musa.nduati <musa.nduati@standardbank.co.za>
2025-05-15 00:58:23 +0200

    loan app system initial commit

---
+++ src/main/java/com/example/loan_payment_system/LoanPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.loan_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loan_payment_system/LoanPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
