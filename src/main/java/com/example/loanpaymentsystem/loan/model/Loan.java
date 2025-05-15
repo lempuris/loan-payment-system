87c2eb1f9bd78256cb2384b1aa745df5e1ad7d4d
musa.nduati <musa.nduati@standardbank.co.za>
2025-05-15 02:23:37 +0200

    added validation for Loan entity

--- src/main/java/com/example/loanpaymentsystem/loan/model/Loan.java
+++ src/main/java/com/example/loanpaymentsystem/loan/model/Loan.java
@@ -2,7 +2,13 @@ package com.example.loanpaymentsystem.loan.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +20,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Loan {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long loanId;
+	@Column(nullable = false)
 	private BigDecimal loanAmount;
+	@Column(nullable = false)
 	private BigDecimal remainingBalance;
+	@Column(nullable = false)
 	private Integer term;
+	@Enumerated(EnumType.STRING)
 	private LoanStatus status;
 	
 	public enum LoanStatus {
